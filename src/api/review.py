66d59dec4bf812086f86cdf975fc049dfdbf1713
dle79 <dle79@calpoly.edu>
2023-11-21 13:37:14 -0800

    edit reviews

--- src/api/review.py
+++ src/api/review.py
@@ -39,17 +39,17 @@ def get_avg_rating_by_recipe(recipe_id: int):
             FROM review
             WHERE recipe_id = :recipe_id
             """
-        ), [{"recipe_id" : recipe_id}]).first()
+        ), [{"recipe_id" : recipe_id}]).scalar_one()
 
         num_reviews = connection.execute(sqlalchemy.text(
             """SELECT COUNT(rating)
             FROM review
             WHERE recipe_id = :recipe_id
             """
-        ), [{"recipe_id" : recipe_id}]).first()
+        ), [{"recipe_id" : recipe_id}]).scalar_one()
 
 
-    return("This recipe has an average rating of " + avg_rating + " stars from " + num_reviews + " reviews.")
+    return(str("This recipe has an average rating of " + avg_rating + " stars from " + num_reviews + " reviews."))
 
 
 
@@ -73,7 +73,7 @@ def get_review_by_recipe(recipe_id: int):
             "review_created": review.review_date})
     
     if review_list == []:
-        return("Recipe " + recipe_id + " does not have any reviews")
+        return(str("Recipe " + recipe_id + " does not have any reviews"))
     return review_list
 
 @router.get("/get_review_by_user")
@@ -97,7 +97,7 @@ def get_review_by_user(user_id: int):
         })
 
     if review_list == []:
-        return("User " + user_id + " did not review any recipes")
+        return(str("User " + user_id + " did not review any recipes"))
     return(review_list)
 
 @router.get("/get_rating_by_user_and_recipe")
@@ -123,7 +123,7 @@ def get_review_by_user(user_id: int, recipe_id: int):
         })
     
     if review_list == []:
-        return("User " + user_id + " did not review recipe " + recipe_id)
+        return(str("User " + user_id + " did not review recipe " + recipe_id))
     return(review_list)
 
 
