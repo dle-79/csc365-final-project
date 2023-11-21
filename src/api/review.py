0469c0807618e8735241fcd804bbbd32cf6db9b9
dle79 <dle79@calpoly.edu>
2023-11-21 12:41:17 -0800

    update recipe

--- src/api/review.py
+++ src/api/review.py
@@ -47,40 +47,76 @@ def get_review_by_recipe(recipe_id: int):
             WHERE recipe_id = :recipe_id
             """
         ), [{"recipe_id" : recipe_id}]).scalar_one()
-    
-    return("This recipe has an average rating of " + avg_rating + " stars from " + num_reviews + " reviews.")
-
-@router.get("/get_rating_by_recipe")
-def get_review_by_recipe(recipe_id: int):
-    with db.engine.begin() as connection:
-        avg_rating = connection.execute(sqlalchemy.text(
-            """SELECT MEAN(rating)
-            FROM review
-            WHERE recipe_id = :recipe_id
-            """
-        ), [{"recipe_id" : recipe_id}]).scalar_one()
-
-        num_reviews = connection.execute(sqlalchemy.text(
-            """SELECT COUNT(rating)
-            FROM review
-            WHERE recipe_id = :recipe_id
-            """
-        ), [{"recipe_id" : recipe_id}]).scalar_one()
 
         reviews = connection.execute(sqlalchemy.text(
-            """SELECT review_description
+            """SELECT review_description, review_date
             FROM review
             WHERE recipe_id = :recipe_id
             """
         ), [{"recipe_id" : recipe_id}]).all()
     
+    if reviews == None:
+        return("No reviews made")
+
     reviews = []
     reviews.append("This recipe has an average rating of " + avg_rating + " stars from " + num_reviews + " reviews.")
 
     for review in reviews:
-        reviews.append(review.review_description)
+        reviews.append({
+            "review": review.review_description,
+            "review_created": review.review_date})
     
     return reviews
+
+@router.get("/get_rating_by_user")
+def get_review_by_user(user_id: int):
+    with db.engine.begin() as connection:
+        reviews = connection.execute(sqlalchemy.text(
+            """SELECT recipe.name AS recipe, recipe_id, rating, review_description, review_date
+            FROM review
+            JOIN recipe
+            ON recipe.recipe_id = review.recipe_id
+            WHERE user_id = :user_id
+            """
+        ), [{"user_id" : user_id}]).all()
+    
+    if reviews == None:
+        return("No reviews made")
+    
+    reviews = []
+    for review in reviews:
+        reviews.append({
+            "recipe": review.recipe,
+            "recipe_id": review.recipe_id,
+            "rating": review.rating,
+            "review": review.review_description,
+            "review_created": review.review_date
+        })
+    
+    return(reviews)
+
+@router.get("/get_rating_by_user_and_recipe")
+def get_review_by_user(user_id: int, recipe_id: int):
+    with db.engine.begin() as connection:
+        reviews = connection.execute(sqlalchemy.text(
+            """SELECT rating, review_description, review_date
+            FROM review
+            WHERE user_id = :user_id AND recipe_id = :recipe_id
+            """
+        ), [{"user_id" : user_id}]).all()
+    
+    if reviews == None:
+        return("No reviews made")
+    
+    reviews = []
+    for review in reviews:
+        reviews.append({
+            "rating": review.rating,
+            "review": review.review_description,
+            "review_date": review.review_date
+        })
+    
+    return(reviews)
 
 
     
